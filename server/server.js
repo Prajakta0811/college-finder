4aa1276bba5539e563f36cb9d3672042af38949c
Prajakta0811 <ppattanaik234@gmail.com>
2026-06-26 21:27:04 +0530

    Prepare for Render deployment

--- server/server.js
+++ server/server.js
@@ -408,12 +408,8 @@ app.get("/api/colleges/:id", async (req, res) => {
    START SERVER
 ========================================================= */
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-  console.log(
-    `Server running on http://localhost:${PORT}`
-  );
-
+  console.log(`Server running on port ${PORT}`);
 });
